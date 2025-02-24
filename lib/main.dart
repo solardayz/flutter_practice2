8cedf6eefcf94832f4664737257c2af13841bd2f
Juna <solardayz@naver.com>
2025-02-25 06:44:08 +0900

    과목 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -31,6 +33,30 @@ class DayOfWeekList extends StatefulWidget {
 class DayOfWeekListState extends State<DayOfWeekList> {
   bool _isBottomSheetVisible = false;
   final GlobalKey _mondayKey = GlobalKey();
+  final List<String> subjects = ['영어', '수학', '국어', '과학', '체육', '음악', '기술'];
+  final List<String> daysOfWeek = ['월', '화', '수', '목', '금'];
+  Map<String, List<String>> timetable = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _generateTimetable();
+  }
+
+  void _generateTimetable() {
+    final random = Random();
+    for (String day in daysOfWeek) {
+      List<String> daySubjects = [];
+      for (int i = 0; i < 10; i++) {
+        if (random.nextBool()) {
+          daySubjects.add(subjects[random.nextInt(subjects.length)]);
+        } else {
+          daySubjects.add(''); // 공백 추가
+        }
+      }
+      timetable[day] = daySubjects;
+    }
+  }
 
   void _showToast(String message) {
     Fluttertoast.showToast(
@@ -44,7 +70,7 @@ class DayOfWeekListState extends State<DayOfWeekList> {
     );
   }
 
-  void _showBottomSheet(BuildContext context) {
+  void _showBottomSheet(BuildContext context, String selectedDay) {
     final RenderBox renderBox =
     _mondayKey.currentContext!.findRenderObject() as RenderBox;
     final Offset offset = renderBox.localToGlobal(Offset.zero);
@@ -75,12 +101,25 @@ class DayOfWeekListState extends State<DayOfWeekList> {
                   child: ListView.builder(
                     itemCount: 10,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        height: 50,
-                        margin: const EdgeInsets.all(8.0),
-                        color: Colors.blue[100],
-                        child: Center(child: Text('Box ${index + 1}')),
-                      );
+                      final String dayKey = selectedDay.substring(0, 1);
+                      if (timetable.containsKey(dayKey) &&
+                          timetable[dayKey] != null &&
+                          index < timetable[dayKey]!.length) {
+                        return Container(
+                          height: 50,
+                          margin: const EdgeInsets.all(8.0),
+                          color: Colors.blue[100],
+                          child: Center(
+                              child: Text(timetable[dayKey]![index])),
+                        );
+                      } else {
+                        return Container(
+                          height: 50,
+                          margin: const EdgeInsets.all(8.0),
+                          color: Colors.grey[300],
+                          child: const Center(child: Text('')),
+                        );
+                      }
                     },
                   ),
                 ),
@@ -110,8 +149,8 @@ class DayOfWeekListState extends State<DayOfWeekList> {
         children: days
             .map((day) => GestureDetector(
           onTap: () {
-            if (day == '월요일') {
-              _showBottomSheet(context);
+            if (daysOfWeek.contains(day.substring(0, 1))) {
+              _showBottomSheet(context, day);
             }
           },
           child: Container(
